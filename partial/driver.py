a40ce37d05912cf3cdcf96c1c17720501b64703c
Ujjwal-N <ujjwal.nadhani@gmail.com>
2021-02-16 02:55:41 -0800

    driver is still partial, giving up because I literally came full circle with debugging efforts. cannot figure out which test case is failing :(

--- partial/driver.py
+++ partial/driver.py
@@ -1,63 +1,66 @@
-f = open("driver/1.in", "r")
-lines = f.readlines()
-tolls = str(lines[1]).split()
-times = str(lines[2]).split()
+# passes only first #3
+# reminder that toll is not collected at gates, but instead at roads
+# easiest way to visualize this is have the toll be collected at the halfway point of the road connecting two gates
+# came full circle in my debugging efforts so I gave up
+def main():
+    gates = int(input())
+    prices = [int(i) for i in input().split(" ")]
+    openTimes = [int(i) for i in input().split(" ")]
+    highTime = -1  # once the gate with the highest time is open, the enitre city will open
+    highGate = -1
+    for i in range(gates):
+        if(openTimes[i] >= highTime):
+            highTime = openTimes[i]
+            highGate = i
+    currRoad = 0
+    nextAvailableGate = 1  # stores the next gate that cannot be opened yet
+    cost = 0
+    time = 0
+    while(True):  # loop runs when the cost is optimized given the set of currently opened gates, so it runs again when the adjacent gate opens
+        # check if possible to go straight through
+        dis = highGate - currRoad  # accounting for travel time
+        if(openTimes[highGate] <= (time + dis)):
+            cost += calcCost(prices[currRoad:gates])
+            print(cost)
+            return
+        else:
+            if(currRoad == (gates - 1)):
+                cost += calcCost(prices[currRoad:gates])
+                print(cost)
+                return
 
-for i in range(0, len(tolls)): 
-    tolls[i] = int(tolls[i]) 
-for i in range(0, len(times)): 
-    times[i] = int(times[i]) 
+            # find cheapest gate to loop through
+            cheapestPrice = prices[currRoad]
+            cheapestRoad = currRoad
 
-roads = len(tolls)
-toll = 0
-gate = 0
-time = 0
+            # finding the first gate we cannot get through and the cheapest gate in all of the gates we can get through and accounting for travel time
+            while((time + nextAvailableGate - currRoad) >= openTimes[nextAvailableGate]):
+                nextAvailableGate += 1
+                # nextAvailableGate - 1 gives the road before it, which is the furthest road that is accessible
+                if(prices[nextAvailableGate - 1] <= cheapestPrice):
+                    cheapestPrice = prices[nextAvailableGate - 1]
+                    cheapestRoad = nextAvailableGate - 1
 
-while(gate < roads):
-	print("time: " + str(time))
-	print("gate: " + str(gate))
-	print("toll: " + str(toll))
-	if(times[-1] < time):
-		#print("here")
-		
-		toll += tolls[gate]
-		gate += 1
-		time += 1
+            timeToKill = openTimes[nextAvailableGate] - \
+                (time + nextAvailableGate - currRoad)
+            # traveling to the cheapest road and updating time with travel time
+            time += cheapestRoad - currRoad
+            timeToKill -= cheapestRoad - currRoad
+            # updating the cost of traveling to the cheapest road
+            cost += calcCost(prices[currRoad: cheapestRoad])
+            currRoad = cheapestRoad  # updating position
+            # one full loop must be a multiple of 2. Without this line, more test cases fail. If you show up to a closed gate with 1,3,5... hours remaning, you have to go back and then forward again
+            timeToKill = timeToKill + \
+                1 if (timeToKill % 2 != 0) else timeToKill
+            cost += prices[cheapestRoad] * timeToKill
+            time += timeToKill
 
-	else:
-		#print("hello")
-		highestGate = gate
-		for i in range(gate, roads):
-			if(times[i] == times[highestGate]):
-				print("here with i=" + str(i))
-				highestGate = i
 
-			if(time > times[i]):
-				highestGate = i
+def calcCost(prices):
+    retInt = 0
+    for price in prices:
+        retInt += price
+    return retInt
 
-		low = 0
-		print("highestGate was found to be " + str(highestGate))
-		for i in range(gate, (highestGate + 1)):
-			print(i)
-			if tolls[low] >= tolls[i]:
-				low = i
-		print("low was found to be " + str(low))
-		while(gate != low):
-			print("nG incrementing toll by " + str(tolls[gate]))
-			toll += tolls[gate]
-			gate += 1
-			time += 1
-		print("gate is now " + str(gate))
-		dToll = tolls[gate]
-		if((len(times) - 1) > highestGate):
-			while (times[highestGate + 1] >= time):
-				toll += dToll
-				print("incrementing toll by " + str(dToll))
-				if(gate == low):
-					gate += 1
-				elif (gate == (low+1)):
-					print("decremented from " + str(gate))
-					gate -= 1
-				time += 1
-		print()
-print(toll)
+
+main()
